8d173d5efc41d3c52e6ec559d122ef38ae965065
Kevin McDonagh <appletvdesign@gmail.com>
2009-01-21 08:51:37 +0000

    Issue 1: There does not seem to be a way to show no cursor in valid css. Also I have had to add the cursor image to the loaded page in order to stop flickering.

--- js/tooltip.js
+++ js/tooltip.js
@@ -3,15 +3,15 @@ function hoverSelectionImg() {
         var listenForHoversClass= "td"; 
         xOffset = 7;
         yOffset = -10;
-        $(listenForHoversClass).hover(function(e){                                                                                       
-               $("body").append("<div id='tooltip' style='display:none;'></div>");
+        $("#customwarsboard td").hover(function(e){                                                                                       
+               $("body").append("<div id='tooltip' style='display:block;'></div>");
                 $("#tooltip")
                         .css("top",(e.pageY - xOffset) + "px")
                         .css("left",(e.pageX + yOffset) + "px")
                         .show();                
     },
         function(){
-                $("#tooltip").remove();
+                
     }); 
         $(listenForHoversClass).mousemove(function(e){
                 $("#tooltip")
