bb90bce6c279bb1a724a7029cbcb8ca97099b726
Kevinf McDonagh <kevin@appletv.co.uk>
2009-04-10 19:00:39 +0000

    Issue 17: removed all the game / fixture specific logic out into separate scripts

--- main/js/lasker-0.1.0.js
+++ main/js/lasker-0.1.0.js
@@ -5,55 +5,6 @@
 $.fn.fileFormat = ".png";
 $.fn.styleName = "lasker";
 $.fn.stylesDir = "../../style";
-$.fn.jsonLocation = "../fixture";
-
-$(document).ready(function(){
-
-	$(window).bind("ajaxError", function() {
-		addToMsgHist("Ajax request unsuccessful");
-	}); 
-
-	snapToMovableTiles();
-	highlightAllowedMoveOnClick();
-	showCoOrdinatesOnTileHover();
-});
-
-function highlightAllowedMoveOnClick()
-{
-	
-//	$("#customwarsboard td").live("click", function(){
-//		cleanAnyVisualState();
-//		
-//		loadJSONmove('../fixture/1/move1.json');
-//		updateCurrentHighlightedTile(this);
-//		logClick(this.id);
-//    });
-	
-	$("#btnNextMove").live("click", function(){
-		cleanAnyOnScreenState();
-		
-		window.moveCount = window.moveCount+1;		
-		loadJSONmove($.fn.jsonLocation + "/1/move"+ window.moveCount + '.json');
-		$("#moveNo").replaceWith("<span id='moveNo'>" + window.moveCount+ "</span>");
-    });
-	
-	$("#btnLastMove").live("click", function(){
-		cleanAnyOnScreenState();
-		
-		if(window.moveCount > 1){
-			window.moveCount = window.moveCount-1;
-			loadJSONmove($.fn.jsonLocation + "/1/move"+ window.moveCount + '.json');
-			$("#moveNo").replaceWith("<span id='moveNo'>" + window.moveCount+ "</span>");
-		}else{
-		
-			if(window.moveCount == 1){		
-				$("#moveNo").replaceWith("<span id='moveNo'>" + 0 + "</span>");
-			}
-		}
-		
-		
-    });
-}
 
 function snapToMovableTiles(){
 	$("img").draggable({ 
@@ -73,32 +24,6 @@ function updateCurrentHighlightedTile(tile){
 	$(tile).addClass('active');
 }
 
-function showCoOrdinatesOnTileHover(){
-	$("#customwarsboard td").hover(function(event){
-		addToMsgs(this.id)
-		currentXY = this.id;
-	}, function(){
-		clrMsgs();
-	});
-}
-
-function addToMsgs(msg){
-	$("#messages").append("<p>" + msg + "</p>");
-}
-
-function logClick(msg){
-	addToMsgHist(msg + " ClickNo: " + window.clickCount);
-	window.clickCount = window.clickCount+1;
-}
-
-function addToMsgHist(msg){
-	$("#messagesHistory").append("<p>" + msg + "</p>");
-}
-
-function clrMsgs(){
-	$("#messages p").remove();
-}
-
 function changeTileImg(tiles){
 	
 	for(tileCoOrds in tiles) {
@@ -107,52 +32,6 @@ function changeTileImg(tiles){
 			$("#" + tileCoOrds).append("<img class='"+ tiles[tileCoOrds] +"' src='"+ $.fn.stylesDir +"/"+$.fn.styleName +"/img/" + tiles[tileCoOrds] + $.fn.fileFormat + "' />");
 		}
 	}
-	
-}
-
-function loadJSONmove(fname){
-    $.getJSON(fname,
-        function(action){
-			
-			if(action.preMove != "" && action.preMove != null){
-				$.fn.preActions(action);
-			}
-
-            if (action.activity == "highlight") {
-                $.fn.highlightTiles(action);
-            }
-
-            if (action.activity == "highlightCastlingKing") {
-                $.fn.highlightCastlingKing(action);
-            }
-			
-            if (action.activity == "replace") {
-                $.fn.replaceTiles(action);
-            }
-			
-			if(action.postMove != "" && action.postMove != null){
-				$.fn.postActions(action);
-			}
-			
-        });
-}
-
-$.fn.preActions = function(action){
-}
-
-$.fn.postActions = function(action){
-	
-	$.each(action.postMove, function(index, postAction){
-		
-		if (postAction.check != "" && postAction.check != null) {
-			addToMsgHist("Check!");
-		}
-		
-		if (postAction.checkmate != "" && postAction.checkmate != null) {
-			addToMsgHist("Check Mate!");
-		}
-			
-	});
 }
 
 $.fn.highlightTiles = function(action){
@@ -178,41 +57,7 @@ $.fn.highlightTiles = function(action){
 	
 };
 
-$.fn.highlightCastlingKing = function(action){
 
-    if(action.actingUnit != "" && action.actingUnit != null){
-        $(".active").removeClass('active');
-        $.each(action.actingUnit, function(index, actingUnit){
-            $("#" + actingUnit.coOrds).addClass('active');
-            $("#" + actingUnit.coOrds).empty();
-            $.fn.addUnitToCoOrds(actingUnit.coOrds, actingUnit.unit, action.actingPlayer, actingUnit.unit);
-        });
-    }
-
-    $.each(action.tiles, function(index, tile){
-        $("#" + tile.coOrds).empty();
-
-        if(tile.unit == null || tile.unit == "" ){
-            $.fn.addActionToCoOrds(tile.coOrds, "highlight", "highlight");
-        }else{
-            $.fn.addUnitToCoOrds(tile.coOrds, 'targetted', tile.occupant, tile.unit);
-        }
-    });
-	
-	$.each(action.target, function(index, tile){
-        $("#" + tile.coOrds).empty();
-
-        if(tile.unit == null || tile.unit == "" ){
-            $.fn.addActionToCoOrds(tile.coOrds, "active", "highlight");
-        }else{
-            $.fn.addUnitToCoOrds(tile.coOrds, "active", tile.occupant, tile.unit);
-			$.each(tile.tiles, function(innerIndex, innerTile){
-		        $("#" + innerTile.coOrds).empty();
-				$.fn.addActionToCoOrds(innerTile.coOrds, "highlight", "secondary-highlight");
-			});
-        }
-    });
-};
 
 $.fn.replaceTiles = function(action){
     
@@ -233,6 +78,29 @@ $.fn.addUnitToCoOrds = function (idCoOrds, imgClass, actingPlayer, actingUnit) {
 $.fn.addActionToCoOrds = function (idCoOrds, imgClass, activity) {
 	$("#" + idCoOrds).append("<img class='"+ imgClass +"' src='"+ $.fn.stylesDir +"/"+$.fn.styleName +"/img/" + activity + $.fn.fileFormat + "' />");
 };
+
+function loadJSONmove(fname){
+    $.getJSON(fname,
+        function(action){
+			
+			if(action.preMove != "" && action.preMove != null){
+				$.fn.preActions(action);
+			}
+
+            if (action.activity == "highlight") {
+                $.fn.highlightTiles(action);
+            }
+			
+            if (action.activity == "replace") {
+                $.fn.replaceTiles(action);
+            }
+			
+			if(action.postMove != "" && action.postMove != null){
+				$.fn.postActions(action);
+			}
+			
+        });
+}
 
 
 
